6bd6944272a7c7e52f4a6a4755087a8cb45ae7c5
Joakin <joaquin@chimeces.com>
2014-01-29 16:03:08 +0100

    publish

--- publish.sh
+++ publish.sh
@@ -7,10 +7,10 @@ rm -rf $TMPDIR/*
 
 cp index.html cnake-prod.js style.css $TMPDIR/
 
-git checkout gh-pages
-cp -r $TMPDIR/* ./
-git add -A
-git commit
+git checkout gh-pages &&
+cp -r $TMPDIR/* ./ &&
+git add -A &&
+git commit &&
 git push origin gh-pages
 
 git checkout master
